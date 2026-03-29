3ae510a34badc7889c536fbac5d3a6f1bef50b24
greeenlaser <sanderveski@gmail.com>
2026-03-29 02:05:19 +0200

    user controls update loop

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -4,16 +4,22 @@
 //Read LICENSE.md for more information.
 
 #include <string_view>
+#include <thread>
+#include <chrono>
 
 #include "core/ee_core.hpp"
+#include "graphics/ee_render.hpp"
 #include "graphics/ee_window.hpp"
 
 #include "core/core.hpp"
 
 using ElypsoEngine::Core::EngineCore;
+using ElypsoEngine::Graphics::Render;
 using ElypsoEngine::Graphics::EngineWindow;
 
 using std::string_view;
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
 
 constexpr string_view programName = "Metal Metropolis";
 
@@ -21,10 +27,15 @@ namespace MetalMetropolis::Core
 {
     void GameCore::Initialize()
     {
-        EngineCore::Initialize(programName, true);
+        EngineCore::Initialize(programName);
 
         EngineWindow::Initialize(programName);
 
-        EngineCore::Update();
+        while (true)
+        {
+            Render::Update();
+
+            sleep_for(milliseconds(1));
+        }
     }
 }

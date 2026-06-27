bb2f238c152ec3f6e9fd65a3c25dfb8f6cca094a
greeenlaser <sanderveski@gmail.com>
2026-06-27 20:08:50 +0300

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,43 @@
 
 #include "math_utils.hpp"
 
-#include "test/crash_test.hpp"
 #include "core/ee_core.hpp"
 #include "graphics/ee_render.hpp"
 #include "graphics/ee_window.hpp"
+#include "core/kw_core.hpp"
+#include "graphics/kw_window.hpp"
 
 using KalaHeaders::KalaMath::vec3;
-
-using MetalMetropolis::Test::AccessViolationType;
-using MetalMetropolis::Test::Crash_AccessViolation;
-using MetalMetropolis::Test::Crash_StackOverflow;
-using MetalMetropolis::Test::Crash_DivideByZero;
 
 using ElypsoEngine::Core::EngineCore;
 using ElypsoEngine::Graphics::Render;
 using ElypsoEngine::Graphics::EngineWindow;
 
+using KalaWindow::Core::KalaWindowCore;
+using KalaWindow::Graphics::ProcessWindow;
+
 using std::string_view;
+using std::to_string;
 using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
 
-constexpr string_view gameTitle = "Metal Metropolis Pre-Release 0004";
+constexpr string_view gameTitle = "Metal Metropolis Pre-Release 1";
 
 int main()
 { 
     EngineCore::Initialize(gameTitle);
-    EngineWindow::Initialize(gameTitle);
+    EngineWindow* ewin = EngineWindow::Initialize(gameTitle);
+
+    ProcessWindow* pw = ProcessWindow::GetRegistry().GetContent(ewin->GetWindowContextID());
+    if (!pw)
+    {
+        KalaWindowCore::ForceClose(
+            "Window error",
+            "Failed to get process window from ID '" + to_string(ewin->GetWindowContextID()) + "'");
+    }
+
+    pw->SetMinSize(800);
+    pw->SetMaxSize(10000);
 
     while (true)
     {

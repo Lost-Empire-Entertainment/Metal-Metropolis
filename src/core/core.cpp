b2550e839c46f7432967e4c22b9c6404adc1de07
greeenlaser <sanderveski@gmail.com>
2026-07-20 23:31:41 +0300

    can toggle vsync and fps cap state

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -11,6 +11,7 @@
 #include "graphics/ee_scene.hpp"
 #include "graphics/kw_window.hpp"
 #include "core/kw_input.hpp"
+#include "core/kg_context.hpp"
 
 using KalaHeaders::KalaLog::Log;
 using KalaHeaders::KalaLog::LogType;
@@ -23,6 +24,8 @@ using ElypsoEngine::Graphics::Scene;
 using KalaWindow::Graphics::ProcessWindow;
 using KalaWindow::Core::Input;
 using KalaWindow::Core::InputCode;
+using KalaGraphics::Core::GraphicsContext;
+using KalaGraphics::Core::VSyncState;
 
 using std::to_string;
 
@@ -33,6 +36,7 @@ static constexpr InputCode ic[] =
 };
 
 Input* input{};
+GraphicsContext* gctx{};
 
 extern const AppConfig ElypsoEngine::Core::appConfig = 
 {
@@ -44,6 +48,7 @@ void ElypsoEngine::Core::Init()
 {
     EngineWindow* ew = EngineWindow::GetRegistry().runtimeContent[0];
     ProcessWindow* pw = ProcessWindow::GetRegistry().GetContent(ew->GetWindowContextID());
+    gctx = GraphicsContext::GetRegistry().GetContent(ew->GetGraphicsContextID());
     input = Input::GetRegistry().GetContent(pw->GetInputID());
 
     pw->SetMinSize({ 800, 600 });
@@ -67,11 +72,43 @@ void ElypsoEngine::Core::Init()
         LogType::LOG_INFO);
 }
 
+void ElypsoEngine::Core::FixedUpdate()
+{
+
+}
+
 void ElypsoEngine::Core::Update()
 {
     if (input->IsComboPressed(ic))
     {
         Log::Print("pressed left ctrl + middle mouse button, calling engine shutdown from game");
         EngineCore::Shutdown();
+    }
+
+    if (input->IsKeyPressed(KeyboardButton::K_1))
+    {
+        Log::Print("@@@@@ set vsync state to off");
+        gctx->SetVSyncState(VSyncState::VSYNC_OFF);
+    }
+    if (input->IsKeyPressed(KeyboardButton::K_2))
+    {
+        Log::Print("@@@@@ set vsync state to adaptive");
+        gctx->SetVSyncState(VSyncState::VSYNC_ON_ADAPTIVE);
+    }
+    if (input->IsKeyPressed(KeyboardButton::K_3))
+    {
+        Log::Print("@@@@@ set vsync state to triple buffered");
+        gctx->SetVSyncState(VSyncState::VSYNC_ON_TRIPLE_BUFFERED);
+    }
+
+    if (input->IsKeyPressed(KeyboardButton::K_4))
+    {
+        Log::Print("@@@@@ set framerate cap state to off");
+        EngineCore::SetFramerateCapState(false);
+    }
+    if (input->IsKeyPressed(KeyboardButton::K_5))
+    {
+        Log::Print("@@@@@ set framerate cap state to on");
+        EngineCore::SetFramerateCapState(true);
     }
 }

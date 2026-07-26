f20b17e33ca9ce838c2ea6dfda33fdd75abb8362
greeenlaser <sanderveski@gmail.com>
2026-07-26 20:29:58 +0300

    get smooth fps

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -3,6 +3,8 @@
 //This is free software, and you are welcome to redistribute it under certain conditions.
 //Read LICENSE.md for more information.
 
+#include <chrono>
+
 #include "log_utils.hpp"
 #include "key_standards.hpp"
 
@@ -16,27 +18,39 @@
 using KalaHeaders::KalaLog::Log;
 using KalaHeaders::KalaLog::LogType;
 using KalaHeaders::KalaKeyStandards::KeyboardButton;
-using KalaHeaders::KalaKeyStandards::MouseButton;
 
+using ElypsoEngine::Core::EngineCore;
 using ElypsoEngine::Core::AppConfig;
 using ElypsoEngine::Graphics::EngineWindow;
-using ElypsoEngine::Graphics::Scene;
 using KalaWindow::Graphics::ProcessWindow;
 using KalaWindow::Core::Input;
-using KalaWindow::Core::InputCode;
 using KalaGraphics::Core::GraphicsContext;
 using KalaGraphics::Core::VSyncState;
 
 using std::to_string;
-
-static constexpr InputCode ic[] = 
-{
-    { .kb = KeyboardButton::K_LEFT_CTRL },
-    { .mb = MouseButton::M_MIDDLE }
-};
+using std::string;
+using std::chrono::time_point;
+using std::chrono::steady_clock;
+using std::chrono::seconds;
+using std::chrono::duration;
+using std::format;
 
 Input* input{};
 GraphicsContext* gctx{};
+
+static string GetFPS(f64 secondsToWait)
+{
+    static f64 cachedFPS = EngineCore::GetCurrentFPS();
+    static time_point start = steady_clock::now();
+
+    if (steady_clock::now() - start > duration<f64>(secondsToWait))
+    {
+        cachedFPS = EngineCore::GetCurrentFPS();
+        start = steady_clock::now();
+    }
+    
+    return format("{:.2f}", cachedFPS);
+}
 
 extern const AppConfig ElypsoEngine::Core::appConfig = 
 {
@@ -50,43 +64,21 @@ void ElypsoEngine::Core::Init()
     ProcessWindow* pw = ProcessWindow::GetRegistry().GetContent(ew->GetWindowContextID());
     gctx = GraphicsContext::GetRegistry().GetContent(ew->GetGraphicsContextID());
     input = Input::GetRegistry().GetContent(pw->GetInputID());
-
-    pw->SetMinSize({ 800, 600 });
-
-    Scene* activeScene = Scene::GetRegistry().GetContent(ew->GetActiveSceneID());
-
-    if (!activeScene)
-    {
-        Log::Print(
-            "Failed to get active scene title because scene '" + to_string(ew->GetActiveSceneID()) + "' was not found!",
-            "MM_MAIN",
-            LogType::LOG_ERROR,
-            2);
-
-        return;
-    }
-
-    Log::Print(
-        "Scene name: " + string(activeScene->GetTitle()),
-        "MM_MAIN",
-        LogType::LOG_INFO);
 }
 
 void ElypsoEngine::Core::FixedUpdate()
 {
+    /*
     Log::Print(
         "This is a fixed update...",
         "MM_MAIN",
-        LogType::LOG_INFO);
+        LogType::LOG_DEBUG);
+    */
 }
 
 void ElypsoEngine::Core::Update()
 {
-    if (input->IsComboPressed(ic))
-    {
-        Log::Print("pressed left ctrl + middle mouse button, calling engine shutdown from game");
-        EngineCore::Shutdown();
-    }
+    Log::Print("fps: " + GetFPS(0.5));
 
     if (input->IsKeyPressed(KeyboardButton::K_1))
     {
@@ -102,16 +94,5 @@ void ElypsoEngine::Core::Update()
     {
         Log::Print("@@@@@ set vsync state to triple buffered");
         gctx->SetVSyncState(VSyncState::VSYNC_ON_TRIPLE_BUFFERED);
-    }
-
-    if (input->IsKeyPressed(KeyboardButton::K_4))
-    {
-        Log::Print("@@@@@ set framerate cap state to off");
-        EngineCore::SetFramerateCapState(false);
-    }
-    if (input->IsKeyPressed(KeyboardButton::K_5))
-    {
-        Log::Print("@@@@@ set framerate cap state to on");
-        EngineCore::SetFramerateCapState(true);
     }
 }
